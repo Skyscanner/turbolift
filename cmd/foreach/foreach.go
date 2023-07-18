cd25d8730368d609300dc4f056be886705263e0c
Sebastien Le Digabel <sledigabel@gmail.com>
2023-07-18 20:30:34 +0100

    clearer code

--- cmd/foreach/foreach.go
+++ cmd/foreach/foreach.go
@@ -47,7 +47,7 @@ MAIN:
 			helpFlag = true
 		default:
 			// we've parsed everything that could be parsed; this is now the command
-			if len(args) > 1 {
+			if len(args[i:]) > 0 {
 				strippedArgs = args[i:]
 			}
 			break MAIN
