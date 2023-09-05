8160462a9f04705308437e6df742a3dd48f5ae4a
Danny Ranson <danny.ranson@skyscanner.net>
2023-09-05 14:34:26 +0100

    use iota for constants

--- internal/github/fake_github.go
+++ internal/github/fake_github.go
@@ -31,18 +31,18 @@ type FakeGitHub struct {
 
 func (f *FakeGitHub) CreatePullRequest(output io.Writer, workingDir string, metadata PullRequest) (didCreate bool, err error) {
 	f.calls = append(f.calls, []string{workingDir, metadata.Title})
-	return f.handler(output, CREATE_PULL_REQUEST, workingDir, "")
+	return f.handler(output, CreatePullRequest, workingDir, "")
 }
 
 func (f *FakeGitHub) ForkAndClone(output io.Writer, workingDir string, fullRepoName string) error {
 	f.calls = append(f.calls, []string{workingDir, fullRepoName})
-	_, err := f.handler(output, FORK_AND_CLONE, workingDir, fullRepoName)
+	_, err := f.handler(output, ForkAndClone, workingDir, fullRepoName)
 	return err
 }
 
 func (f *FakeGitHub) Clone(output io.Writer, workingDir string, fullRepoName string) error {
 	f.calls = append(f.calls, []string{workingDir, fullRepoName})
-	_, err := f.handler(output, CLONE, workingDir, fullRepoName)
+	_, err := f.handler(output, Clone, workingDir, fullRepoName)
 	return err
 }
 
@@ -50,7 +50,7 @@ func (f *FakeGitHub) ClosePullRequest(output io.Writer, workingDir string, branc
 	// TODO: handle this differently; branchName here is replacing fullRepoName
 	// This is OK for now because fullRepoName is used nowhere in the github mocks
 	f.calls = append(f.calls, []string{workingDir, branchName})
-	_, err := f.handler(output, CLOSE_PULL_REQUEST, workingDir, branchName)
+	_, err := f.handler(output, ClosePullRequest, workingDir, branchName)
 	return err
 }
 
@@ -65,7 +65,7 @@ func (f *FakeGitHub) GetPR(output io.Writer, workingDir string, _ string) (*PrSt
 
 func (f *FakeGitHub) GetDefaultBranchName(output io.Writer, workingDir string, fullRepoName string) (string, error) {
 	f.calls = append(f.calls, []string{workingDir, fullRepoName})
-	_, err := f.handler(output, GET_DEFAULT_BRANCH_NAME, workingDir, fullRepoName)
+	_, err := f.handler(output, GetDefaultBranchName, workingDir, fullRepoName)
 	return "main", err
 }
 
@@ -115,7 +115,7 @@ func NewAlwaysReturnsFalseFakeGitHub() *FakeGitHub {
 
 func NewAlwaysFailsOnGetDefaultBranchFakeGitHub() *FakeGitHub {
 	return NewFakeGitHub(func(output io.Writer, command Command, workingDir string, fullRepoName string) (bool, error) {
-		if command == GET_DEFAULT_BRANCH_NAME {
+		if command == GetDefaultBranchName {
 			return false, errors.New("synthetic error")
 		}
 		return true, nil
@@ -127,9 +127,9 @@ func NewAlwaysFailsOnGetDefaultBranchFakeGitHub() *FakeGitHub {
 type Command int
 
 const (
-	FORK_AND_CLONE          Command = 0
-	CLONE                   Command = 1
-	CREATE_PULL_REQUEST     Command = 2
-	CLOSE_PULL_REQUEST      Command = 3
-	GET_DEFAULT_BRANCH_NAME Command = 4
+	ForkAndClone Command = iota
+	Clone
+	CreatePullRequest
+	ClosePullRequest
+	GetDefaultBranchName
 )
