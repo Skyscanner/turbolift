bbd931763e59ad9aecc47100805cb7da004c73ba
Jesús Carpintero <jesus.carpintero@gmail.com>
2024-04-30 13:51:12 +0200

    update string builder

--- cmd/foreach/foreach.go
+++ cmd/foreach/foreach.go
@@ -16,6 +16,7 @@
 package foreach
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"strings"
@@ -100,7 +101,7 @@ func run(c *cobra.Command, args []string) {
 
 	for i := range args {
 		if strings.Contains(args[i], " ") {
-			args[i] = "\"" + args[i] + "\""
+			args[i] = fmt.Sprintf(`"%s"`, args[i])
 		}
 	}
 	command := strings.Join(args, " ")
